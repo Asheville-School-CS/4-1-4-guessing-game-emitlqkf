ffa8bc9cdd692e1cb99efd4e714440d7e2ea4e5d
emitlqkf <maww22@ashevilleschool.org>
2020-10-20 11:12:29 -0400

    guessing game

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -7,6 +7,22 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
+        System.out.println("guess a number from 0 to 100 ");
+        int number = scan.nextInt();
+        int random = (int)(Math.random() * 100);
+
+        // (int) (Math.random() * (upper - lower) + lower);
+
+        while(number != random){
+            if(number > random){
+                System.out.println("Too high!");
+            }
+            if(number < random){
+                System.out.println("Too low!");
+            }
+            number = scan.nextInt();
+        }
+            System.out.println("You got it!");
 
         // Ask the user to guess a number from 0 to 100
         // Get the first guess using scan.nextInt();
